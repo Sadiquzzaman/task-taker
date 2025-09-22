cfff32757965e088d35efa4d585a6e7f2faa7c42
Shovon <sadiq.zaman@shohoz.com>
2025-09-23 00:00:51 +0600

    swagger setup is done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+    imports: [
+      ConfigModule.forRoot({ isGlobal: true }), // load .env globally
+    ],
+    controllers: [AppController],
+    providers: [AppService],})
 export class AppModule {}
